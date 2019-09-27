b77aea8f107acba7a406b90d8f1f3abe1ccf06b2
Cale Teeter <cale.teeter@microsoft.com>
2019-09-27 10:02:39 -0400

    Sample for open default browser options

--- src/commands/sampleCommand.ts
+++ src/commands/sampleCommand.ts
@@ -1,5 +1,19 @@
 import * as vscode from 'vscode';
+import * as opn from 'opn';
 
 export async function sampleCommand(): Promise<void> {
     vscode.window.showInformationMessage('sample command called.');
+}
+
+export async function sampleCommandOption1(): Promise<void> {
+    vscode.env.openExternal(vscode.Uri.parse('https://docs.openzeppelin.com/contracts/2.x/'));
+}
+
+export async function sampleCommandOption2(): Promise<void> {
+    vscode.window.showInformationMessage('Explore more information about the contracts downloaded', 
+        'More details', 'Ignore').then(selection => {
+            if (selection === 'More details') {
+                opn('https://docs.openzeppelin.com/contracts/2.x/');
+            }
+        });
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import { ext } from './extensionVariables';
 import { AzureUserInput } from 'vscode-azureextensionui';
-import { sampleCommand } from './commands/sampleCommand';
+import { sampleCommand, sampleCommandOption1, sampleCommandOption2 } from './commands/sampleCommand';
 
 export function activate(context: vscode.ExtensionContext) {
     ext.context = context;
@@ -9,4 +9,6 @@ export function activate(context: vscode.ExtensionContext) {
     ext.ui = new AzureUserInput(context.globalState);
 
     vscode.commands.registerCommand('sample.sampleCommand', sampleCommand);
+    vscode.commands.registerCommand('sample.sampleCommand1', sampleCommandOption2);
+    vscode.commands.registerCommand('sample.sampleCommand2', sampleCommandOption1);
 }
